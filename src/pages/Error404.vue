0d25a45cdcadabd8e797f97f7f5f0c9e92696cc9
Pedro Beani <32439211+pBeani@users.noreply.github.com>
2021-09-21 21:59:06 -0300

    adicionar estilo para a tela de 404

--- src/pages/Error404.vue
+++ src/pages/Error404.vue
@@ -1,21 +1,20 @@
 <template>
-  <div class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center">
+  <div class="fullscreen bg-primary text-dark text-center q-pa-lg flex flex-center error-404">
     <div>
-      <div>
+      <div class="text-h4 error-404__code">
         404
       </div>
 
-      <div class="text-h2">
-        The dimension you're looking for doesn't yet exists.
+      <div class="text-h4 error-404__message">
+        The dimension you're looking for is not real.
       </div>
 
       <q-btn
-        class="q-mt-xl"
-        color="white"
-        text-color="blue"
-        unelevated
+        class="q-mt-xl text-h6 q-px-xl error-404__button"
+        color="dark"
+        text-color="primary"
         to="/"
-        label="Return from whence thou cam'st"
+        label="Return to your galaxy"
         no-caps
       />
     </div>
@@ -29,3 +28,19 @@ export default defineComponent({
   name: 'Error404',
 });
 </script>
+
+<style lang="scss" scoped>
+@media only screen and (max-width: $breakpoint-xs) {
+  .error-404 {
+    &__code {
+      font-size: 1.5rem;
+    }
+    &__message {
+      font-size: 1.5rem;
+    }
+    &__button {
+      font-size: 1rem;
+    }
+  }
+}
+</style>
